48d02a4e0788a53f4c7b5a02463963a204a3d0a7
Bryan Marty <bryan@bryanmarty.com>
2025-06-25 06:50:31 +0000

    Initial commit
    Signed-off-by: Bryan Marty <bryan@bryanmarty.com>

---
+++ .chezmoiscripts/run_once_install-starship.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# https://github.com/batistein/dotfiles/blob/master/.chezmoiscripts/run_once_install-starship.sh
+
+if  command -v starship >/dev/null 2>&1; then 
+    echo "starship already installed"
+else
+    curl -sS https://starship.rs/install.sh | sh -s -- --bin-dir ~/.local/bin
+fi
