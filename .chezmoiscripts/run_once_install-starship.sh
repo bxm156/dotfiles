6529411ed498a9eee90d84d7f94a39afd8a76cad
Bryan Marty <bryan@bryanmarty.com>
2025-11-07 01:23:23 +0000

    fix bug

--- .chezmoiscripts/run_once_install-starship.sh
+++ .chezmoiscripts/run_once_install-starship.sh
@@ -5,5 +5,6 @@
 if  command -v starship >/dev/null 2>&1; then
     echo "starship already installed"
 else
+    mkdir -p ~/.local/bin
     curl -sS https://starship.rs/install.sh | sh -s -- --bin-dir ~/.local/bin -y
 fi
