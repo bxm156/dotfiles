16336fd08bff68cf1691defa41f8b387cb002bee
Bryan Marty <bryan@bryanmarty.com>
2025-12-09 06:23:42 -0800

    Start tracking initial komorebi and whkd

---
+++ dot_config/komorebi/komorebi_start.ps1
@@ -0,0 +1,4 @@
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\bryan\.config\komorebi'
+$Env:WHKD_CONFIG_HOME = 'C:\Users\bryan\.config\whkd'
+
+komorebic.exe start --whkd --bar
---
+++ dot_config/komorebi/komorebi_stop.ps1
@@ -0,0 +1,1 @@
+komorebic.exe stop --whkd --bar
